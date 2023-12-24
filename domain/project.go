8c2d9f26d54b6acdffe9750ee794f75d5fdf5465
Rafael Bodill <justrafi@gmail.com>
2023-12-24 12:48:25 +0200

    feat(domain): improve repo handling and theme

--- domain/project.go
+++ domain/project.go
@@ -16,3 +16,46 @@ type Project struct {
 
 // ProjectList is a list of projects with keys.
 type ProjectListKeyed map[string]Project
+
+func (p Project) GetRepo(name, prefix string) (Repository, bool) {
+	for _, repo := range p.Repos {
+		if prefix+repo.GetName() == name {
+			return repo, true
+		}
+	}
+	for _, subProj := range p.SubProjects {
+		subPrefix := prefix + subProj.Name + "/"
+		if r, found := subProj.GetRepo(name, subPrefix); found {
+			return r, true
+		}
+	}
+	return Repository{}, false
+}
+
+func (p Project) GetAllRepos(prefix ...string) []Repository {
+	var names []Repository
+	if len(prefix) == 0 {
+		prefix = []string{""}
+	}
+	names = append(names, p.Repos...)
+	for _, subProj := range p.SubProjects {
+		subPrefix := prefix[0] + subProj.Name + "/"
+		names = append(names, subProj.GetAllRepos(subPrefix)...)
+	}
+	return names
+}
+
+func (p Project) ListReposWithNamespace(prefix ...string) []string {
+	var names []string
+	if len(prefix) == 0 {
+		prefix = []string{""}
+	}
+	for _, repo := range p.Repos {
+		names = append(names, prefix[0]+repo.GetName())
+	}
+	for _, subProj := range p.SubProjects {
+		subPrefix := prefix[0] + subProj.Name + "/"
+		names = append(names, subProj.ListReposWithNamespace(subPrefix)...)
+	}
+	return names
+}
--- domain/settings.go
+++ domain/settings.go
@@ -13,24 +13,38 @@ type Style struct {
 }
 
 type Theme struct {
+	// General
+	Normal        Style `json:"normal,omitempty"`
+	Bullet        Style `json:"bullet,omitempty"`
+	PreviewHeader Style `json:"previewHeader,omitempty"`
+
+	// Project
+	ProjectTitle Style `json:"projectTitle,omitempty"`
+	Provider     Style `json:"provider,omitempty"`
+	Desc         Style `json:"desc,omitempty"`
+
+	// Repository
+	RepoTitle Style `json:"repoTitle,omitempty"`
+	RepoPath  Style `json:"repoPath,omitempty"`
+	GitOutput Style `json:"gitOutput,omitempty"`
+
+	// Branch
+	BranchName      Style `json:"branchName,omitempty"`
+	BranchCurrent   Style `json:"branchCurrent,omitempty"`
+	BranchIndicator Style `json:"branchIndicator,omitempty"`
+	RemoteName      Style `json:"remoteName,omitempty"`
+
+	// Tag
+	TagIndicator Style `json:"tagIndicator,omitempty"`
+
 	// Status
 	Modified  Style `json:"modified,omitempty"`
 	Untracked Style `json:"untracked,omitempty"`
 	Diff      Style `json:"diff,omitempty"`
 	Error     Style `json:"error,omitempty"`
 
-	// Project
-	ProjectTitle Style `json:"projectTitle,omitempty"`
-	Bullet       Style `json:"bullet,omitempty"`
-	Provider     Style `json:"provider,omitempty"`
-	Desc         Style `json:"desc,omitempty"`
-
-	// Repository
-	RepoTitle Style `json:"repoTitle,omitempty"`
-	GitOutput Style `json:"gitOutput,omitempty"`
-
 	// Table
-	// TableBorder      Style `json:"tableBorder,omitempty"`
+	// TableBorder      Style `json:"tableBorder,omitempty"` FIXME:
 	TableBorderStyle Style `json:"tableBorderStyle,omitempty"`
 	TableHeader      Style `json:"tableHeader,omitempty"`
 	TableRowEven     Style `json:"tableRowEven,omitempty"`
